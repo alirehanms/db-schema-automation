5fd33a32edf694300c2c6acf7f8a7e7fa57261b4
alirehanms <ali6777622@gmail.com>
2025-08-07 12:09:43 +0000

    Update ws-client.js

--- ws-client.js
+++ ws-client.js
@@ -2,7 +2,12 @@ const WebSocket = require('ws');
 const { spawn } = require('child_process');
 const ws = new WebSocket('ws://localhost:8081');
 ws.on('open', () => console.log('DB client connected'));
-ws.on('message', () => {
-  console.log('Deploy signal received');
-  spawn('bash', ['start/deploy.sh'], { cwd: __dirname, stdio: 'inherit' });
+ws.on('message', (data) => {
+  console.log('Message from server:', data.toString());
+
+  if (data.toString() === 'deploy') {
+    console.log('Deploy signal received');
+    spawn('bash', ['start/deploy.sh'], { cwd: __dirname, stdio: 'inherit' });
+  }
 });
+
